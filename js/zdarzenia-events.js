ca3c3a76293b3b5ec39a498bc57f0be8d8bafbcc
Paweł Białek <pawelbialek23@op.pl>
2018-02-17 15:53:16 +0100

    another commit

--- js/zdarzenia-events.js
+++ js/zdarzenia-events.js
@@ -1,16 +1,42 @@
 'use strict';
 
-if (e == null) //(!e) 
-{
-    var e = window.event;
-}
+//if (e == null) //(!e) 
+//{
+//    var e = window.event;
+//}
 
 var secondLink = document.getElementsByTagName('a')[1]; //tworzymy zmienną z drugim linkiem
 
+var thirdLink = document.getElementsByTagName('a')[2]; //tworzymy zmienną z trzecim linkiem
+
 function alarm(e) {
-    e.preventDefeult(); //zapobiegamy domyślenj akcji
-    
+    e.preventDefault(); //zapobiegamy domyślenj akcji
+
     console.log('kliknięto kolejny link');
-    
+    thirdLink.style.backgroundColor = 'red';
     console.log(e.type);
 }
+
+secondLink.onclick = alarm; //wywołujemy funkcję alarm() na drugim linku po kliknięciu
+
+
+
+thirdLink.addEventListener('click', alarm);
+
+thirdLink.removeEventListener('click', alarm);
+
+//Efekt powiększenia napisu po najechaniu
+var mainHeader = document.getElementById('main-header');
+//Przypisujemy nagłówek strony do zmiennej
+
+function resize(e) { //przekazujemy zdarzenia jako parametr funkcji
+    if (e.type == 'mouseover') { //sprawdzanie typu zdarzenia 
+        mainHeader.style.fontSize = '70px'; // przywróć domyślną wielkość po zjechaniu
+    } else {
+        mainHeader.style.fontSize = 'inherit' // przywrócenie domyślnej wielkości po zjechaniu
+    }
+
+}
+
+mainHeader.onmouseover = resize;
+mainHeader.onmouseout = resize;
